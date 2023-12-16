cbc4d576e5b3c923938274336003fc1f878b2ea2
Artegro <95859890+Artegro@users.noreply.github.com>
2023-12-16 21:18:42 +0300

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -20,7 +20,6 @@ public class WelcomerTest {
 	@Test
 	public void welcomerSaysHunter() {
 		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
 		assertThat(welcomer.sayHunter(), containsString("hunter"));
 	}
 	@Test
